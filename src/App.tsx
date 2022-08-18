c11e7a2dfaefe2716ceeded3b106d1b8d5b4cae2
Carla Pico <carlaspico@gmail.com>
2022-08-18 10:57:32 -0400

    imported the models component from the backend file and added a pages component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Feed from './pages/Feed';
 
 function App() {
+  //pull the ig-backend photos from localhost:5001
+  useEffect( () => {}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Feed /> 
       </header>
     </div>
   );
---
+++ src/pages/Feed.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Photo } from "../models/photo"
+import "./Feed.css"
+
+function Feed () {
+    return (
+        <>
+            <h1> The Feed </h1>
+        </>
+    )
+}
+
+export default Feed
